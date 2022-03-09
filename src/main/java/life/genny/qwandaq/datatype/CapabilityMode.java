8ac6c1c02e544f13aa73607897a4107497c20067
Jasper Robison <jasper.robison22@gmail.com>
2022-03-09 14:26:18 +1100

    constructor + docs

--- src/main/java/life/genny/qwandaq/datatype/CapabilityMode.java
+++ src/main/java/life/genny/qwandaq/datatype/CapabilityMode.java
@@ -4,14 +4,42 @@ import java.io.Serializable;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
+/*
+ * An enum class used to represent the ability in regards to a capability
+ * 
+ * @author Adam Crow
+ */
 @RegisterForReflection
 public enum CapabilityMode implements Serializable {
 
+	/**
+	 * represents no capability in regards to something
+	 */
 	NONE("NONE", 0),
+
+	/**
+	 * Represents the capability to view something
+	 */
 	VIEW("VIEW", 1),
+
+	/**
+	 * Represents the capability to edit something
+	 */
 	EDIT("EDIT", 2),
+
+	/**
+	 * Represents the capability to add something
+	 */
 	ADD("ADD", 3),
+
+	/**
+	 * Represents the capability to delete something
+	 */
 	DELETE("DELETE", 4),
+
+	/**
+	 * Represents the capability to do something to self
+	 */
 	SELF("SELF", 5);
 
 	private final String name;
