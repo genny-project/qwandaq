fbff083d0902134c923577a6d918da65f313d037
Adam Crow <adamcrow63@gmail.com>
2022-03-23 18:28:06 +1100

    Added default constructor for QEvetMessage

--- src/main/java/life/genny/qwandaq/message/QEventMessage.java
+++ src/main/java/life/genny/qwandaq/message/QEventMessage.java
@@ -2,44 +2,47 @@ package life.genny.qwandaq.message;
 
 import java.io.Serializable;
 
-
 public class QEventMessage extends QMessage implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	private static final String MESSAGE_TYPE = "EVT_MSG";
 	private String event_type;
 	public MessageData data;
-	
-	/** 
+
+	public QEventMessage() {
+		super();
+	}
+
+	/**
 	 * @return String
 	 */
 	@Override
 	public String toString() {
 		return "QEventMessage [event_type=" + event_type + ", data=" + data + "]";
 	}
-	
-	/** 
+
+	/**
 	 * @return String
 	 */
 	public String getEvent_type() {
 		return event_type;
 	}
-	
-	/** 
+
+	/**
 	 * @param event_type the event type to set
 	 */
 	public void setEvent_type(String event_type) {
 		this.event_type = event_type;
 	}
-	
-	/** 
+
+	/**
 	 * @return MessageData
 	 */
 	public MessageData getData() {
 		return data;
 	}
-	
-	/** 
+
+	/**
 	 * @param data the message data to set
 	 */
 	public void setData(MessageData data) {
@@ -49,11 +52,11 @@ public class QEventMessage extends QMessage implements Serializable {
 	public QEventMessage(String eventType) {
 		super(MESSAGE_TYPE);
 	}
-	
+
 	public QEventMessage(String eventType, String code) {
 		super(MESSAGE_TYPE);
 		this.event_type = eventType;
 		this.data = new MessageData(code);
 	}
-	
+
 }
