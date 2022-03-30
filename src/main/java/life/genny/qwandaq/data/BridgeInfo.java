5b761c8d4d1ec73730db32484cbe08e38ef58ee1
Jasper Robison <jasper.robison22@gmail.com>
2022-03-30 17:23:08 +1100

    register for reflection

--- src/main/java/life/genny/qwandaq/data/BridgeInfo.java
+++ src/main/java/life/genny/qwandaq/data/BridgeInfo.java
@@ -3,14 +3,17 @@ package life.genny.qwandaq.data;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 /**
  * A Bridge Info storage class used with BridgeSwitch to map individual user bridge Ids
  * 
  * @author Jasper Robison
  */
+@RegisterForReflection
 public class BridgeInfo {
 
-	public BridgeInfo() {}
-
 	public ConcurrentMap<String, String> mappings = new ConcurrentHashMap<>();
+
+	public BridgeInfo() {}
 }
