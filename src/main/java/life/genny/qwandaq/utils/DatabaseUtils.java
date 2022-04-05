707cf84f5c7632ae9a603ca7e3bdd1f3067a9712
Jian Li <hbjoylee@gmail.com>
2022-04-05 10:07:26 +1000

    fix query

--- src/main/java/life/genny/qwandaq/utils/DatabaseUtils.java
+++ src/main/java/life/genny/qwandaq/utils/DatabaseUtils.java
@@ -135,7 +135,7 @@ public class DatabaseUtils {
 
 		try {
 			Query query = entityManager
-					.createQuery("FROM Attribute WHERE realm=:realmStr AND name not like 'App\\_% order by id'",
+					.createQuery("FROM Attribute WHERE realm=:realmStr AND name not like 'App\\_%' order by id",
 							Attribute.class)
 					.setParameter("realmStr", realm);
 
